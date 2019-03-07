6e497e34674bca9ed916ea68e7b4bfc3191e17de
Arne Rossmann <arne.rossmann@capgemini.com>
2019-03-07 17:00:13 +0100

    first working version

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "node_weatherifo",
+  "version": "1.0.0",
+  "description": "Node.js weather app on Docker",
+  "author": "Arne Rossmann <arne.rossmann@gmail.com>",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.16.4"
+  }
+}
