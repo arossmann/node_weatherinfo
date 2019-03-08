d1f9bff4efce4c4bb9138fb543adae075b2ded95
Arne Rossmann <arne.rossmann@capgemini.com>
2019-03-08 19:23:03 +0100

    added partials and weather icon

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const port = 8080;
 const request = require('request');
 let apiKey = process.env.APIKEY;
 app.set('apiKey', apiKey);
-//const apiKey = '##################';
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.set('view engine', 'ejs')
+app.use('/public', express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  res.render('pages/index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
@@ -20,14 +19,18 @@ app.post('/', function (req, res) {
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 request(url, function (err, response, body) {
     if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+      res.render('pages/index', {weather: null, error: 'Error, please try again'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+        res.render('pages/index', {weather: null, error: 'Error, please try again'});
       } else {
+        var iconCode = weather.weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        //console.log("weather:"+iconCode+" "+iconUrl)
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        //$(".icon").html("<img class = 'newIcon' src=" + iconUrl + ">");
+        res.render('pages/index', {weather: weatherText, weatherIcon: iconUrl, error: null});
       }
     }
   });
