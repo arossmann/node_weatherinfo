11347fc0b1c1c2124f5833dc70e6df4fa36a174a
Arne Rossmann <arne.rossmann@capgemini.com>
2020-10-06 20:51:34 +0200

    reorganized files and adopted to new expressJS version

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('pages/index', {weather: null, error: null});
+  res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
@@ -19,17 +19,17 @@ app.post('/', function (req, res) {
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 request(url, function (err, response, body) {
     if(err){
-      res.render('pages/index', {weather: null, error: 'Error, please try again'});
+      res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
-        res.render('pages/index', {weather: null, error: 'Error, please try again'});
+        res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         var iconCode = weather.weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         //console.log("weather:"+iconCode+" "+iconUrl)
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('pages/index', {weather: weatherText, weatherIcon: iconUrl, error: null});
+        res.render('index', {weather: weatherText, weatherIcon: iconUrl, error: null});
       }
     }
   });
